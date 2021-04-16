6d732204657263b2574b40561005ce90fd62c28e
Auke Speksnijder <Auke.Speksnijder@triodos.com>
2021-04-16 16:36:29 +0200

    testcafe test

---
+++ testcafe/test.js
@@ -0,0 +1,8 @@
+import { Selector } from 'testcafe';
+
+fixture `Getting Started`
+    .page `https://bartosz-angular-uitgewerkt.stackblitz.io`;
+
+test('My first test', async t => {
+    // Test code
+});
